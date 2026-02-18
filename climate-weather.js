2a201a33a6fc7aa42d50018aad87609f18dc8f9a
Edison Stha <shresthaedison98@gmail.com>
2026-02-18 18:08:48 +0545

    update the weather

--- climate-weather.js
+++ climate-weather.js
@@ -1,43 +1,42 @@
 export let climate = [
-    {
-        
-        weathers: 'rainny',
-        images: './Pic/weather/rainny.png'
-    },
-    {
-        weathers: 'sunny',
-        images: './Pic/weather/sunny.png'
-    },
-    {
-        weathers: 'lighting-windy-rainny',
-        images: './Pic/weather/lighting-windy-ranny.png'
-    },
-    {
-        weathers: 'rainny-sunny-cloudy',
-        images: './Pic/weather/rainny-sunny-cloudy.png'
-    },
-    {
-        weathers: 'sunny-cloudy',
-        images: './Pic/weather/sunny-cloudy.png'
-    },
-    {
-        weathers: 'windy-rainny',
-        images: './Pic/weather/windy-rainnny.png'
-    },
-    {
-        weathers: 'windy-sunny',
-        images: './Pic/weather/windy-sunny.png'
-    },
-    {
-        weathers: 'clear-night',
-        images: './Pic/weather/clear-night.png' 
-    },
-    {
-        weathers: 'clouds-night',
-        images: './Pic/weather/cloudy-night.png'
-    },
-    {
-        weathers: 'smoke-cloud',
-        images: './Pic/weather/smoke-cloud.png'
-    },
+  {
+    weathers: 'rain',
+    images: './Pic/weather/rainny.png'
+  },
+  {
+    weathers: 'clear',
+    images: './Pic/weather/sunny.png'
+  },
+  {
+    weathers: 'lighting-windy-rainny',
+    images: './Pic/weather/lighting-windy-ranny.png'
+  },
+  {
+    weathers: 'rainny-sunny-cloudy',
+    images: './Pic/weather/rainny-sunny-cloudy.png'
+  },
+  {
+    weathers: 'cloud',
+    images: './Pic/weather/sunny-cloudy.png'
+  },
+  {
+    weathers: 'wind',
+    images: './Pic/weather/windy-rainnny.png'
+  },
+  {
+    weathers: 'wind',
+    images: './Pic/weather/windy-sunny.png'
+  },
+  {
+    weathers: 'clear',
+    images: './Pic/weather/clear-night.png'
+  },
+  {
+    weathers: 'cloud',
+    images: './Pic/weather/cloudy-night.png'
+  },
+  {
+    weathers: 'smoke',
+    images: './Pic/weather/smoke-cloud.png'
+  },
 ];
